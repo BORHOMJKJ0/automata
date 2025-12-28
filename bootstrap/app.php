6b62665c390227ca8f89b099309d7882e7cb90ae
BORHOMJKJ0 <hborhom89@gmail.com>
2025-12-29 01:08:38 +0300

    fix deploy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,29 +1,40 @@
 <?php
 
-// api/index.php - For Laravel 10 and below
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+*/
 
-// Set correct paths for Vercel
-$_SERVER['SCRIPT_FILENAME'] = __DIR__.'/../public/index.php';
-$_SERVER['SCRIPT_NAME'] = '/index.php';
-$_SERVER['PHP_SELF'] = '/index.php';
-$_SERVER['DOCUMENT_ROOT'] = __DIR__.'/../public';
-
-// Change to project root
-chdir(__DIR__.'/..');
-
-// Load composer autoload
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel application
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-// Create kernel and handle request
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+$app = new Illuminate\Foundation\Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
-$response->send();
+/*
+|--------------------------------------------------------------------------
+| Bind Important Interfaces
+|--------------------------------------------------------------------------
+*/
 
-$kernel->terminate($request, $response);
+$app->singleton(
+    Illuminate\Contracts\Http\Kernel::class,
+    App\Http\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+/*
+|--------------------------------------------------------------------------
+| Return The Application
+|--------------------------------------------------------------------------
+*/
+
+return $app;
